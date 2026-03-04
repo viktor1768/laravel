3f93bf320694112c592a49743499574a1efaf86b
Viktor <viktor22derishev@gmail.com>
2026-03-03 21:14:31 -0800

    use collection split

--- app/Http/Controllers/ColController.php
+++ app/Http/Controllers/ColController.php
@@ -5,9 +5,9 @@
 		public function index()
 		{
 			$collection = collect([1, 2, 3,4,5,6,7,8,921,3,21,2,34,1]);
-			$diff = $collection->diff([1,2,3,4,8,1]);
+			$groups = $collection->split($collection->count()/2);
 			dd([
-				'average' => $diff,
+				'average' => $groups,
 				'collection' => $collection,
 				'title' => 'Заголовок',
 			]);
