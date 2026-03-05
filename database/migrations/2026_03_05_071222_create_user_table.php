a465489edd5288b01da7e3ae66a03b7d4000537c
Viktor <viktor22derishev@gmail.com>
2026-03-04 23:13:40 -0800

    migration user table

--- database/migrations/2026_03_05_071222_create_user_table.php
+++ database/migrations/2026_03_05_071222_create_user_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-		Schema::table('posts', function (Blueprint $table) {
-			$table->string('title');
-			$table->text('desc');
-		});
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('user');
     }
 };
