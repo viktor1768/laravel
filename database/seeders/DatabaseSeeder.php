70304f99aec99cb1e1a51c0282ae9bcca2d44297
Viktor <viktor22derishev@gmail.com>
2026-03-12 02:29:28 -0700

    12 march

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,17 +8,37 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
+use PostSeeder;
+use CommentSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
     use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call([
+            PostSeeder::class,
+            CommentSeeder::class,
+        ]);
+
+            DB::table('users')->insert([
+				[
+				'name' => Str::random(10),
+				'email' => Str::random(10).'@gmail.com',
+				'password' => Hash::make('12345'),
+				],  
+			]);
+
+
+    }
+}
+class PostSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    public function run(): void
+    {
 
             DB::table('users')->insert([
 				[
