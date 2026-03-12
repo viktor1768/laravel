c92821939716c134a92a9f183f370a5f1ffb6e6d
Viktor <viktor22derishev@gmail.com>
2026-03-12 01:27:07 -0700

    add default value

--- database/migrations/2026_03_12_082432_column_salary_user.php
+++ database/migrations/2026_03_12_082432_column_salary_user.php
@@ -12,11 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('user', function (Blueprint $table) {
-            $table->id();
-            $table->text('name');
-            $table->text('surname');
-            $table->dateTime('birthday');
-            $table->timestamps();
+            $table->integer('salary')->comment('default value')->default(0);
         });
     }
 
