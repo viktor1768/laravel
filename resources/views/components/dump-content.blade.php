9e38191510437fc77d40b304ba62f05f2220b176
Viktor <viktor22derishev@gmail.com>
2026-03-17 21:31:32 -0700

    dump table users

--- resources/views/components/dump-content.blade.php
+++ resources/views/components/dump-content.blade.php
@@ -6,7 +6,18 @@
 	<title>{{$titling}}</title>
 </head>
 <body>
-	<p>{{$users}}</p>
+<table>
+	@foreach($users as $user)
+	<tr>
+		<td style = "border: 1px solid #000000">{{$user->id}}</td>
+		<td style = "border: 1px solid #000000">{{$user->name}}</td>
+		<td style = "border: 1px solid #000000">{{$user->email}}</td>
+		<td style = "border: 1px solid #000000">{{$user->age}}</td>
+		<td style = "border: 1px solid #000000">{{$user->salary}}</td>
+		<td style = "border: 1px solid #000000">{{$user->password}}</td>
+	</tr>
+	@endforeach
+</table>
 </body>
 </html>
 	
