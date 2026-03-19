a604a30a56549c1009d51d46961e7c50d2886fa4
Viktor <viktor22derishev@gmail.com>
2026-03-18 23:43:12 -0700

    add new user

--- app/Http/Controllers/DumpController.php
+++ app/Http/Controllers/DumpController.php
@@ -5,7 +5,14 @@
 	{
 		public function dumparr()
 		{
-			$users = DB::table('users')->whereBetween('age', [20,70])->skip(5)->take(3)->get();
+			$users = DB::table('users')->get();
+			$ids = DB::table('users')->insert([
+				'name' => 'sdadsa',
+				'email' => '231321sad',
+				'age' => 15,
+				'salary' => 21323,
+				'password' => '2313dsfklnzc',
+			]);
 			return view('components.dump-content', [
 				'users' => $users,
 				'titling' => 'page',
@@ -13,4 +20,3 @@
 		}
 	}
 ?>	
-	
