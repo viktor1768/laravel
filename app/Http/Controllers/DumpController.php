e49cf94b97986f528663272586e07804cefd87be
Viktor <viktor22derishev@gmail.com>
2026-03-18 23:13:22 -0700

    use odrderby desc select

--- app/Http/Controllers/DumpController.php
+++ app/Http/Controllers/DumpController.php
@@ -5,7 +5,7 @@
 	{
 		public function dumparr()
 		{
-			$users = DB::table('users')->orderBy('age')->get();
+			$users = DB::table('users')->orderBy('salary', 'desc')->get();
 			return view('components.dump-content', [
 				'users' => $users,
 				'titling' => 'page',
