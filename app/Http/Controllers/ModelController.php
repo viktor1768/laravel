db61ede97e4a30e151c387c16d30f9f3714b6938
Viktor <viktor22derishev@gmail.com>
2026-04-09 01:38:37 -0700

    use Exception

--- app/Http/Controllers/ModelController.php
+++ app/Http/Controllers/ModelController.php
@@ -1,15 +1,17 @@
 <?php
 	namespace App\Http\Controllers;
 	use App\Models\Post;
+	use Illuminate\Database\Eloquent\ModelNotFoundException;
 	use App\Http\Controllers\Controller;
 
 	class ModelController extends Controller
 	{
 		public function index($order, $dir = 'desc')
 		{
-			$posts = Post::where('id','>', 0)->orderBy($order, $dir)->get();
+			$posts = Post::where('id','>', 30)->findOrFail(1);
 
 			return view('components.model-content', ['posts'=> $posts]);
+			
 			
 			
 		}
